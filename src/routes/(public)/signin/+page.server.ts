916095aaab2b26eae25584f362f4ad4574fe4adb
Hiranga <hiranga.g@gmail.com>
2024-10-30 00:31:40 +1100

    ADD db accessed from event.locals

--- src/routes/(public)/signin/+page.server.ts
+++ src/routes/(public)/signin/+page.server.ts
@@ -3,3 +3,126 @@ import type { PageServerLoad } from './$types';
 export const load = (async () => {
     return {};
 }) satisfies PageServerLoad;
+
+
+/** @satisfies {import('./$types').Actions} */
+export const actions = {
+
+    registerRoot: async (event) => {
+        console.log("registerRoot")
+        try {
+            const db = await event.locals.sdb;
+
+            const res = await db.signin({
+                username: 'hiranga',
+                password: 'VerySecurePassword!',
+            });
+
+            console.log({ res })
+        } catch (error) {
+            console.log(error)
+        }
+    },
+
+    register: async (event) => {
+        console.log("register")
+        try {
+            const db = await event.locals.sdb;
+
+            const res = await db.signup(
+                {
+                    access: "user",
+                    // scope: "public",
+                    // Provide the variables used by the signup query
+                    variables: {/*  */
+                        name: 'foo',
+                        // username: 'Mary117',
+                        email: 'foo@doe.org',
+                        password: 'VerySecurePassword!',
+                        // name: 'John Doe',
+                        // username: 'John117',
+                        // email: 'john@doe.org',
+                        // password: 'VerySecurePassword!',
+                    }
+                }
+                //     {
+                //     username: 'hiranga',
+                //     password: 'VerySecurePassword!',
+                // }
+            );
+            console.log({ res })
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    signin: async (event) => {
+        console.log("signin")
+        try {
+            const db = await event.locals.sdb;
+
+            const res = await db.signin(
+                {
+                    // namespace: "test2",
+                    // database: "test2",
+                    access: "user",
+                    variables: {
+                        name: 'Mary Doe',
+                        // username: 'Mary117',
+                        email: 'Mary@doe.org',
+                        password: 'VerySecurePassword!',
+                    }
+                }
+            );
+            console.log(JSON.stringify(res, null, 2))
+        } catch (error) {
+            console.log(error)
+        }
+    },
+
+    // ACTIONS
+    createDummy: async (event) => {
+        console.log("createDummy")
+        try {
+            const db = await event.locals.sdb;
+
+            const res = await db.query_raw(
+                /*surql*/ `CREATE dummy 
+                SET name = time::now(),
+                user = $auth.id;
+                `
+            );
+            console.log(JSON.stringify(res, null, 2))
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    getDummy: async (event) => {
+        console.log("getDummy")
+        try {
+            const db = await event.locals.sdb;
+
+            const res = await db.query_raw(
+                /*surql*/ `SELECT * FROM dummy;`
+            );
+            console.log(JSON.stringify(res, null, 2))
+        } catch (error) {
+            console.log(error)
+        }
+    },
+
+    getSession: async (event) => {
+        console.log("getSession")
+        try {
+            const db = await event.locals.sdb;
+
+            const res = await db.query(
+                /*surql*/ `SELECT * FROM $session;`
+            );
+
+            // console.log({ res })
+            console.log(JSON.stringify(res, null, 2))
+        } catch (error) {
+            console.log(error)
+        }
+    }
+};
--- src/routes/(public)/signin/+page.svelte
+++ src/routes/(public)/signin/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { Button } from '$lib/components/ui/button';
 	import { Input } from '$lib/components/ui/input';
 	import { sdbConnect_user, surrealClient } from '$lib/db/surreal';
 	import type { PageData, RequestEvent } from './$types';
@@ -30,19 +31,41 @@
 	// }
 
 	let token = $state();
-	const user_login = async (email: string, name: string, password: string) => {
-		console.log('login');
-		// const token = await surrealClient?.signin({
-		token = await surrealClient?.signin({
-			access: 'user',
-			variables: {
-				email,
-				name,
-				password
-			}
-		});
+	const user_signin = async (email: string, name: string, password: string) => {
+		console.log('signin');
 
-		console.log(JSON.stringify(token, null, 2)); //NOT WORKING
+		if (surrealClient) {
+			// const token = await surrealClient?.signin({
+			token = await surrealClient.signin({
+				access: 'user',
+				variables: {
+					email,
+					name,
+					password
+				}
+			});
+			console.log('token:', JSON.stringify(token, null, 2)); //NOT WORKING
+		} else {
+			console.error('surrealClient is not connected');
+		}
+	};
+
+	const user_signup = async (email: string, name: string, password: string) => {
+		console.log('signup');
+		if (surrealClient) {
+			const res = await surrealClient.signup({
+				access: 'user',
+				variables: {
+					email,
+					name,
+					password
+				}
+			});
+
+			console.log(JSON.stringify(res, null, 2)); //NOT WORKING
+		} else {
+			console.error('surrealClient is not connected');
+		}
 	};
 
 	// store the token in the browser cookie
@@ -50,7 +73,57 @@
 
 Token: {token}
 
+<div class="grid grid-flow-row gap-5">
+	<div class="flex flex-col gap-1">
+		register
+		<!-- <form  method="POST" action="/test?/register"> -->
+		<form method="POST" action="?/register">
+			<!-- <form  method="POST" action="?/registerRoot"> -->
+			<!-- <Input type="text" name="name" placeholder="Name" />
+			<Input type="email" name="email" id="email_register" />
+			<Input type="password" name="password" id="password_register" /> -->
+
+			<Button type="submit">Register</Button>
+		</form>
+	</div>
+	<div class="flex flex-col gap-1">
+		signin
+		<!-- <form  method="POST" action="/test?/register"> -->
+		<form method="POST" action="?/signin">
+			<!-- <form  method="POST" action="?/registerRoot"> -->
+			<!-- <Input type="text" name="name" placeholder="Name" />
+			<Input type="email" name="email" id="email_signin" />
+			<Input type="password" name="password" id="password_signin" /> -->
+
+			<Button type="submit">signin</Button>
+		</form>
+	</div>
+	<div class="flex flex-col gap-1">
+		getSession
+		<form method="POST" action="?/getSession">
+			<Button type="submit">getSession</Button>
+			<!-- <Button  method="POST" formaction="?/getSession">getSession</Button> -->
+		</form>
+	</div>
+	ACTIONS =====================
+	<div class="flex flex-col gap-1">
+		<form method="POST" action="?/createDummy">
+			<Button type="submit">createDummy</Button>
+			<!-- <Button  method="POST" formaction="?/getSession">getSession</Button> -->
+		</form>
+	</div>
+	<div class="flex flex-col gap-1">
+		<form method="POST" action="?/getDummy">
+			<Button type="submit">getDummy</Button>
+			<!-- <Button  method="POST" formaction="?/getSession">getSession</Button> -->
+		</form>
+	</div>
+</div>
+
+<!-- 
 <Input bind:value={email} />
 <Input bind:value={name} />
 <Input bind:value={password} />
-<button onclick={() => user_login(email, name, password)}> Login </button>
+<Button onclick={() => user_signin(email, name, password)}>Signin</Button>
+
+<Button onclick={() => user_signup(email, name, password)}>Signup</Button> -->
